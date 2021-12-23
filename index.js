f58e9df7bcded730cb3b50a66edbc5db4742978b
Pong En-Yu <andrew0911114933@gmail.com>
2021-12-23 10:27:33 +0800

    Add some transition

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ var fsm = new StateMachine({
 bot.on('message', function (event) {
   // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
   event.reply(`你說了 ${event.message.text}`);
+
+  if (fsm.is(solid)) {
+    fsm.melt();
+    event.reply(`融化ㄌ`);
+  } else if (fsm.is(liquid)) {
+    fsm.vaporize();
+    event.reply(`蒸發ㄌ`);
+  } else if (fsm.is(gas)) {
+    fsm.condence();
+    event.reply(`凝結ㄌ`);
+  }
+  
 });
 
 // Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
