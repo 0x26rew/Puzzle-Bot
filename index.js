374b51a0e910e028397ca7d80b1ff5cacb689c34
Pong En-Yu <andrew0911114933@gmail.com>
2021-12-23 10:34:34 +0800

    Add some bug

--- index.js
+++ index.js
@@ -30,17 +30,17 @@ bot.on('message', function (event) {
   // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
   event.reply(`你說了 ${event.message.text}`);
 
-  if (fsm.is(solid)) {
+  if (fsm.is('solid')) {
     fsm.melt();
     event.reply(`融化ㄌ`);
-  } else if (fsm.is(liquid)) {
+  } else if (fsm.is('liquid')) {
     fsm.vaporize();
     event.reply(`蒸發ㄌ`);
-  } else if (fsm.is(gas)) {
+  } else if (fsm.is('gas')) {
     fsm.condence();
     event.reply(`凝結ㄌ`);
   }
-  
+
 });
 
 // Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
